6e19c76dae4d958f54b47d2c1b334cef79c2434c
HariyaNobuki <HariyaNobuki@users.noreply.github.com>
2022-08-06 18:28:16 +0900

    OK

--- optimizer.py
+++ optimizer.py
@@ -136,6 +136,95 @@ class ParticleSwarmOptimization:
                     self.Vs[i][j] = -self.Vmax
 
 
+class RBF_ParticleSwarmOptimization:
+
+    def __init__(self, N, W, C1, C2, Xmax, Vmax, MAX_EVALUATIONS, PROB_DIMEINTION):
+        # PSO settings
+        self.N    = N     # Number of particles
+        self.W    = W     # Inertia Weight
+        self.C1   = C1    # Cognitive Learning factor
+        self.C2   = C2    # Social Learning factor
+        self.Vmax = Vmax  # Maximum velocity
+        self.Xmax = Xmax  # Defined range of position variables (search space)
+
+        #Problem settings
+        self.MAX_EVALUATIONS = MAX_EVALUATIONS 
+        self.PROB_DIMEINTION = PROB_DIMEINTION 
+
+        #Particle variables
+        self.Xs        = [None for _ in range(self.N)] # Position
+        self.Fs        = [None for _ in range(self.N)] # Fitness (personal best's fitness)
+        self.Ps        = [None for _ in range(self.N)] # Personal best's position
+        self.Vs        = [None for _ in range(self.N)] # Velocity
+
+        self.BestX     = None # Global best
+        self.BestFX    = None # Best fitness (of global best)
+
+        ARCHIVE_X = np.array([])
+        ARCHIVE_F = np.array([])
+        ARCHIVE_POP = []
+        ARCHIVE_DIV = []
+        fit_list = []
+        fit_dict = pd.DataFrame()
+
+        aVar = ARCHIVE_X
+        aObj = ARCHIVE_F
+        data = np.vstack((aVar.T, aObj.T))
+        rbf = Rbf(*data, function='cubic')
+
+        
+    def initialization(self):
+        for i in range(self.N):            
+            self.Xs[i] = np.random.rand(self.PROB_DIMEINTION) *  (2* self.Xmax) - self.Xmax
+            self.Vs[i] = np.random.rand(self.PROB_DIMEINTION) *  (2* self.Vmax) - self.Vmax
+
+    def init_evaluate(self, prob):
+        for i in range(self.N):
+            # ind evaluate
+            tmp = prob.evaluate(self.Xs[i])
+            # ind update
+            if self.Fs[i] == None or tmp <= self.Fs[i]:
+                self.Fs[i] = tmp
+                self.Ps[i] = [self.Xs[i][j] for j in range(self.PROB_DIMEINTION)]
+            # pop update
+            if self.BestFX == None or tmp <= self.BestFX:
+                self.BestFX = tmp
+                self.BestX = [self.Xs[i][j] for j in range(self.PROB_DIMEINTION)]
+
+    def evaluate(self, prob):
+        for i in range(self.N):
+            # ind evaluate
+            tmp = prob.evaluate(self.Xs[i])
+            # ind update
+            if self.Fs[i] == None or tmp <= self.Fs[i]:
+                self.Fs[i] = tmp
+                self.Ps[i] = [self.Xs[i][j] for j in range(self.PROB_DIMEINTION)]
+            # pop update
+            if self.BestFX == None or tmp <= self.BestFX:
+                self.BestFX = tmp
+                self.BestX = [self.Xs[i][j] for j in range(self.PROB_DIMEINTION)]
+
+    def generation(self):
+         for i in range(self.N):
+             for j in range(self.PROB_DIMEINTION):
+                self.Xs[i][j] = self.Xs[i][j] + self.Vs[i][j]
+                # bounding 
+                if self.Xs[i][j] > self.Xmax:
+                    self.Xs[i][j] = self.Xmax
+                if self.Xs[i][j] < -self.Xmax:
+                    self.Xs[i][j] = -self.Xmax
+
+    def update(self):   # velocity update
+        for i in range(self.N):
+            for j in range(self.PROB_DIMEINTION):
+                self.Vs[i][j] = self.W * self.Vs[i][j] + self.C1 * np.random.rand()*(self.Ps[i][j] - self.Xs[i][j]) + self.C2 * np.random.rand()*(self.BestX[j] - self.Xs[i][j])
+                
+                if self.Vs[i][j] > self.Vmax:
+                    self.Vs[i][j] = self.Vmax
+                if self.Vs[i][j] < -self.Vmax:
+                    self.Vs[i][j] = -self.Vmax
+
+
 # Do not change
 def run(problem, optimizer, MAX_EVALUATIONS, opt,filename , trial):
     evals = 0
@@ -206,6 +295,15 @@ if __name__ == "__main__":
 
     # make files
     makefiles()
+
+    # RBF_PSO setting
+    W, C1, C2, Vmax = 0.4, 2.0, 2.0, 20
+    for i in range(len(PROBLEM_LIST)):
+        for trial in range(NUM_TRIAL):
+            np.random.seed(trial)
+            PSO = RBF_ParticleSwarmOptimization(N, W, C1, C2, Xmax, Vmax, MAX_EVALUATIONS, PROB_DIMEINTION)
+            fnc = function.Function(PROBLEM_LIST[i], PROB_DIMEINTION)
+            run(fnc, PSO, MAX_EVALUATIONS, "PSO","PSO_{}".format(PROBLEM_LIST[i]),trial)
 
     #Random search setting
     for i in range(len(PROBLEM_LIST)):
